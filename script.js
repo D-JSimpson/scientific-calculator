6dc6afcac9719262552176fd006745a75a7344b1
D-JSimpson <simpsondante03@gmail.com>
2022-03-02 18:46:11 -0500

    Add a button that recreates the input field.
    It has all of the same functions of the original and when the div
    overflows, a scrollbar appears that is forced to the bottom.

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
+const userInputDisplayContainer = document.getElementById("user-input-display-container");
+
 const userInputField = document.querySelector('input');
 userInputField.addEventListener('keyup', e => {
- console.log("the cursor is at: ", e.target.selectionStart);
- console.log(e.target.keyCode);
+    let currentPosition = e.target.selectionStart;
+ console.log("the cursor is at: ", currentPosition);
 });
 userInputField.addEventListener('click', e => {
-    console.log("the cursor is at: ", e.target.selectionStart);
+    let currentPosition = e.target.selectionStart;
+    console.log("the cursor is at: ", currentPosition);
 });
+
+//placeholder button for enter. When the user presses enter and the calculations run, a new box will appear with all the functions of the starting input field
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(){
+let userInputField = document.createElement('input');
+
+ userInputField.classList.add("user-input-display");
+
+ userInputField.addEventListener('keyup', e => {
+    let currentPosition = e.target.selectionStart;
+ console.log("the cursor is at: ", currentPosition);
+});
+
+userInputField.addEventListener('click', e => {
+    let currentPosition = e.target.selectionStart;
+    console.log("the cursor is at: ", currentPosition);
+});
+
+ userInputField.onkeydown = function(event){return checkUserKey(event)};
+
+ userInputDisplayContainer.appendChild(userInputField);
+ userInputDisplayContainer.scrollTop = userInputDisplayContainer.scrollHeight - userInputDisplayContainer.clientHeight; //force scrollbar to the bottom
+});
+
 //Limit the users input to just numbers without using type = "Number". That breaks the event listeners for the userInputField
 function checkUserKey(event){
     let char = event.keyCode;
-    if(char > 31 && char < 58 || char == 8 || char == 61 || char == 173){
+    if(char > 31 && char < 58 || char == 8 || char == 61 || char == 173 || char == 191){
         return true;
     }
     return false;
 }
-//Allow other input values as well, such as "+" or "()"
