dd67a42f897b45b811640b31d9e6def7fcf433ba
D-JSimpson <simpsondante03@gmail.com>
2022-05-20 17:19:42 -0400

    Add file which will handle the placement of operations in input field.

---
+++ operations.js
@@ -0,0 +1,9 @@
+//Intended Functionality
+//--------------------
+//The division operator loops through its parent element
+//Does not stop until it hits an operation or reaches the end of the parent element
+//Puts all of the looped through elements on top, moves cursor to bottom.
+const divButton = document.getElementById("division");
+function divisionCreator(){
+
+}
