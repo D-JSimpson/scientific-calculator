bd0d1c65a2e0b43317b49761543b12181e30e9a7
D-JSimpson <simpsondante03@gmail.com>
2022-05-20 18:40:28 -0400

    Add function that create a division container
    Have it place inside the input field

--- operations.js
+++ operations.js
@@ -4,6 +4,26 @@
 //Does not stop until it hits an operation or reaches the end of the parent element
 //Puts all of the looped through elements on top, moves cursor to bottom.
 const divButton = document.getElementById("division");
+divButton.addEventListener("click", function(){divisionCreator();});
+
 function divisionCreator(){
+    let divisionContainer = document.createElement("span");
+        divisionContainer.classList.add("division-container");
+    let numerator = document.createElement("span");
+        numerator.classList.add("numerator");
+    let denominator = document.createElement("span");
+        denominator.classList.add("denominator");
+    divisionContainer.appendChild(numerator);
+    divisionContainer.appendChild(denominator);
+    const cursor = document.querySelector(".cursor");
+    cursor.insertAdjacentElement("afterend", divisionContainer);
+    console.log("hi")
+}
+//Only if empty
+function divisionUpdateCursor(pos){
+    if(pos.classList[0] == "numerator" || pos.classList[0] == "denominator"){
+        pos.insertAdjacentHTML("afterbegin", "<span class='cursor blink'></span>");
+        cursorBlink();
+    }
 
 }
